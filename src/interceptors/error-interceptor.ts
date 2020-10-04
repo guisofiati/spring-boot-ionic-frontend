522724625c8ca8326e8e35d6d21ee65ced8ca011
Sofiati <gui_sf@hotmail.com>
2020-10-03 23:46:01 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,12 +1,15 @@
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { AlertController } from "ionic-angular";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(public storage: StorageService){
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController){
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
@@ -24,10 +27,17 @@ export class ErrorInterceptor implements HttpInterceptor{
             console.log("Erro detectado pelo interceptor:");
             console.log(errorObj);
 
-            switch(errorObj.status){
+            switch(errorObj.status) {
+                case 401:
+                this.handle401();
+                break;
+
                 case 403: 
                 this.handle403();
                 break;
+
+                default:
+                this.handleDefaultEror(errorObj);    
             }
 
             return Observable.throw(errorObj);
@@ -36,6 +46,34 @@ export class ErrorInterceptor implements HttpInterceptor{
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: Falha de autenticação',
+            message: 'Email e/ou senha incorreto(s)',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultEror(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
