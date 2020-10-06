f987759660d0a899d0da8de14578829300c523fc
Sofiati <gui_sf@hotmail.com>
2020-10-06 16:49:40 -0300

    Acesso ao carrinho: botao flutuante e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,10 +21,10 @@ export class MyApp {
     public auth: AuthService ) {
     this.initializeApp();
 
-    // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: ''}
     ];
 
@@ -32,8 +32,6 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
